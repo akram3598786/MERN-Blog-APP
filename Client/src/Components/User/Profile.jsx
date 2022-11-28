57a4eaf84a0f1da41b71e09b3b1549dfe999ed7b
akram3598786 <akramali03598786@gmail.com>
2022-11-28 17:06:43 +0530

    show user profile

--- Client/src/Components/User/Profile.jsx
+++ Client/src/Components/User/Profile.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import "./Profile.css"
 
 
-export default function Profile(){
+export default function Profile() {
+
+    const [user, setuser] = useState({});
+
+    useEffect(() => {
+        getUserData();
+    }, []);
+
+    const getUserData =async () => {
+        let userData = await JSON.parse(localStorage.getItem("LoggedUser"));
+        setuser(userData);
+    }
+
 
     return (
 
         <>
-        <h1>User Profile</h1>
+            <h1>User Profile</h1>
+            
+            <div id="userDetail">
+                <img src="https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/approved-profile-icon.svg" alt="" />
+               <h2>{user.name}</h2>
+               <p>EMAIL : {user.email}</p>
+               <p>MOBILE : {user.mobile}</p>
+            </div>
         </>
     )
 }
