da48a9d12a7428afc0fc8763b66fa98539a1f77a
akram3598786 <akramali03598786@gmail.com>
2022-11-28 15:58:01 +0530

    Blog details page  done

--- Client/src/Components/Blogs/BlogDetails.jsx
+++ Client/src/Components/Blogs/BlogDetails.jsx
@@ -1,14 +1,52 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 
 
 export default function BlogDetails(){
 
+    const [loading, setloading] = useState(false);
+    const [error, setError] = useState(false);
+    const [blog, setblog] = useState({});
     const {blogId} = useParams();
-    console.log(blogId);
+
+    useEffect(() => {
+        setloading(true);
+        getAllBlogs();
+    }, [])
+
+    const getAllBlogs = () => {
+        let userData = JSON.parse(localStorage.getItem("LoggedUser"));
+        console.log(userData._id);
+        let url = `http://127.0.0.1:8080/post/${blogId}`;
+        axios.get(url).
+            then((res) => {
+                console.log(res.data)
+                setblog(res.data)
+            }).
+            catch((err) => {
+                console.log(err)
+                setError(true)
+            }).
+            finally((res) => setloading(false));
+    }
+    
+    //console.log(blogId);
     return (
         <>
-        <h1>Blog</h1>
-        
+        <h1>Blog View</h1>
+            <div>
+                {
+                    loading ? <h1>Loading...</h1> :
+                        error ? <h1>Error : Something Went Wrong</h1> :
+                          <>
+                           <h1>{blog.title}</h1>
+                           <p>{blog.description}</p>
+                           </>
+
+                }
+
+            </div>
         </>
     )
 }
