972736c4072fec2d16015b1c861b88cf24ad82f5
akram3598786 <akramali03598786@gmail.com>
2022-11-29 17:11:46 +0530

    added delete function

--- Config/ConnectDB.js
+++ Config/ConnectDB.js
@@ -3,9 +3,9 @@ const username = encodeURIComponent("akram2407");
 const password = encodeURIComponent("Akram@3598786");
 
 async function ConnectDB(req, res){
-  // let url =  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/MERN-APP";
-  let url =  `mongodb+srv://${username}:${password}@mern-app-blog.sgvzudd.mongodb.net/?retryWrites=true&w=majority`
-   || "mongodb://127.0.0.1:27017/MERN-APP";
+  // let url =  "mongodb://127.0.0.1:27017/MERN-APP";
+   let url =  `mongodb+srv://${username}:${password}@mern-app-blog.sgvzudd.mongodb.net/?retryWrites=true&w=majority`
+    || "mongodb://127.0.0.1:27017/MERN-APP";
     
   return new Promise((resolve, reject)=>{
     mongoose.connect(url).
--- Controllers/Posts.js
+++ Controllers/Posts.js
@@ -3,56 +3,69 @@ const express = require("express");
 const UserModel = require("../Models/User.model");
 const PostRouter = express.Router();
 
-async function CreatePost(req, res){
-    try{
+async function CreatePost(req, res) {
+    try {
         const payload = req.body;
-         const userId = payload.createdby;
-         let user = await UserModel.findById(userId);
+        const userId = payload.createdby;
+        let user = await UserModel.findById(userId);
         // console.log(user);
-        if(user){
+        if (user) {
             let post = await PostModel(payload);
-            await post.save();  
+            await post.save();
             user.posts = [...user.posts, post._id];
             await user.save();
-           res.status(201).send({message : "Post created", post});
-        }else{
+            res.status(201).send({ message: "Post created", post });
+        } else {
             res.status(404).send("User not exist");
         }
     }
-    catch(err){
+    catch (err) {
         res.status(500).send(err.message);
     }
 }
 
-async function getALLposts(req, res){
-    try{
-        const {userId} = req.params;
-       let posts = await PostModel.find({createdby : userId});
-       if(posts.length > 0){
-          res.status(200).send(posts);
-       }else{
-         res.status(404).send("No post exist");
-       }
+async function getALLposts(req, res) {
+    try {
+        const { userId } = req.params;
+        let posts = await PostModel.find({ createdby: userId });
+        if (posts.length > 0) {
+            res.status(200).send(posts);
+        } else {
+            res.status(404).send("No post exist");
+        }
     }
-    catch(err){
+    catch (err) {
         res.send(err.message);
     }
 }
 
-async function getSinglePost(req, res){
-    try{
-       let {postId} = req.params;
-       let post = await PostModel.findById(postId);
-       if(post) res.status(200).send(post);
-       else res.status(404).send("Post not found !");
+async function getSinglePost(req, res) {
+    try {
+        let { postId } = req.params;
+        let post = await PostModel.findById(postId);
+        if (post) res.status(200).send(post);
+        else res.status(404).send("Post not found !");
     }
-    catch(err){
+    catch (err) {
         res.send(err.message);
+    }
+}
+
+async function deletePost(req, res) {
+    try {
+        let { postId } = req.params;
+
+        let post = await PostModel.deleteOne({_id : postId});
+      //  console.log(post)
+        res.status(200).send("deleted");
+    } catch (err) {
+        res.status(400).send({ msg: "Error occured", Error: err.message });
     }
 }
 
 PostRouter.post("/:userId", CreatePost);
 PostRouter.get("/:userId/all", getALLposts);
 PostRouter.get("/:postId", getSinglePost);
+ PostRouter.delete("/:postId", deletePost);
 
-module.exports =  PostRouter;
+module.exports = PostRouter;
