ce9de063ed193407f9b2806027812bf627ee7d7c
akram3598786 <akramali03598786@gmail.com>
2022-12-05 10:31:58 +0530

    edited env variable

--- Config/ConnectDB.js
+++ Config/ConnectDB.js
@@ -3,7 +3,7 @@ const username = encodeURIComponent("akram2407");
 const password = encodeURIComponent("Akram@3598786");
 
 async function ConnectDB(req, res){
-    let url = "mongodb://127.0.0.1:27017/MERN-APP" || process.env.MONGODB_URI
+    let url = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/MERN-APP"
    // console.log("uri",process.env.MONGODB_URI);
   return new Promise((resolve, reject)=>{
     mongoose.connect(url).
